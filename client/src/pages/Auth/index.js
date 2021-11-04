82a31f834396f254cf6c107a614fc84ed39f4013
Yuvrajhere <yuvrajisbest13@gmail.com>
2021-11-04 10:00:29 +0530

    Initial commit

---
+++ client/src/pages/Auth/index.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Login from "../../components/Login";
+import Signup from "../../components/Signup";
+import { useParams } from "react-router-dom";
+
+const Auth = () => {
+  let { type } = useParams();
+
+  return (
+    <div className="Auth">{type === "login" ? <Login /> : <Signup />}</div>
+  );
+};
+
+export default Auth;
